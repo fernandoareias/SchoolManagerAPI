ac0f4d3ceb815770ee6aacd73c6be0894981acfe
Fernando <nando.calheirosx@gmail.com>
2021-12-18 15:08:36 -0300

    Base Arch

---
+++ src/1.2.2 - SchoolManager.Service.Course.AppService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _1._2._2___SchoolManager.Service.Course.AppService
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/1.2.5.1 - SchoolManager.Service.Course.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _1._2._5._1___SchoolManager.Service.Course.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
