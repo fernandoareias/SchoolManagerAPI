7b33bd1f7d90b609034716569e489261cabcd901
Fernando <nando.calheirosx@gmail.com>
2021-12-18 23:54:50 -0300

    CourseAPI Tests Base

---
+++ src/SchoolManager.Services.Course.Tests/CourseTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace SchoolManager.Services.Course.Tests
+{
+    public class CourseTest
+    {
+
+        [Fact]
+        public void ShouldReturnWhenIsValid()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void ShouldNotReturnWhenIsInvalid()
+        {
+            Assert.True(false);
+        }
+
+
+    }
+}
