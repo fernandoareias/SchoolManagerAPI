3a0e3814a2673b28c15aa9cb254e3c70a9e14f59
Fernando <nando.calheirosx@gmail.com>
2021-12-20 21:37:34 -0300

    Base HttpService

---
+++ src/SchoolManager.Services.Teacher.Domain/Teacher/Interfaces/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using SchoolManager.Services.Teacher.Domain.Teacher.HttpActions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Services.Teacher.Domain.Teacher.Interfaces
+{
+    public interface IBaseService : IDisposable
+    {
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
