a0373eecc67da01836ee3d09385b1787cae94dac
Fernando <nando.calheirosx@gmail.com>
2021-12-20 21:34:16 -0300

    Tests

---
+++ src/SchoolManager.Services.Teacher.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SchoolManager.Services.Teacher.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
