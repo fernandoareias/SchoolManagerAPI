f149c5da1896d3095cf7f1c364ad0e8dd9e0cef1
Fernando <nando.calheirosx@gmail.com>
2021-12-20 21:34:05 -0300

    Value Objects

---
+++ src/SchoolManager.Services.Teacher.Domain/Teacher/VOs/Email.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Services.Teacher.Domain.Teacher.VOs
+{
+    [Owned]
+    public class Email : ValueObject
+    {
+        public Email(string address)
+        {
+            Address = address;
+        }
+
+        private Email()
+        {
+
+        }
+        [Column("Address")]
+        [Required(ErrorMessage = "Address is required!")]
+        [EmailAddress(ErrorMessage = "Address invalid!")]
+        [MaxLength(125, ErrorMessage = "The address should be a maximum of 125 characters.")]
+        public string Address { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Address;
+        }
+    }
+}
