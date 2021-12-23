c74c318f8eec310827cc5c46b68aa70a084aa548
Fernando <nando.calheirosx@gmail.com>
2021-12-23 12:05:48 -0300

    Dtos

--- src/SchoolManager.Services.Teacher.Domain/Teacher/DTOs/TeacherCreateDto.cs
+++ src/SchoolManager.Services.Teacher.Domain/Teacher/DTOs/TeacherCreateDto.cs
@@ -11,10 +11,6 @@ namespace SchoolManager.Services.Teacher.Domain.Teacher.DTOs
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
-        public string Street { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
+        public TeacherAddressDto Address { get; set; }
     }
 }
