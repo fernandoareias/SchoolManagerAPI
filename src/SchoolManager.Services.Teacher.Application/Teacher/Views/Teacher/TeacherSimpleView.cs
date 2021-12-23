9ae17ec15e634fef27bb17e6489aedec913dcf1e
Fernando <nando.calheirosx@gmail.com>
2021-12-23 12:06:38 -0300

    Views

---
+++ src/SchoolManager.Services.Teacher.Application/Teacher/Views/Teacher/TeacherSimpleView.cs
@@ -0,0 +1,27 @@
+﻿using SchoolManager.Services.Teacher.Application.Teacher.Views.Course;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager.Services.Teacher.Application.Teacher.Views.Teacher
+{
+    public class TeacherSimpleView
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+        public CourseSimpleView Course { get; set; }
+
+        public TeacherSimpleView(Domain.Teacher.Entities.Teacher teacher, Domain.Teacher.Entities.Course course)
+        {
+            Id = teacher.Id;
+            Name = teacher.Name.ToString();
+            
+            Course = new CourseSimpleView(course);
+        }
+    }
+}
